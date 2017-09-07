2da0042563d7c3f08af3f44a063387624ecf8147
maydma <demi_ma@sina.com>
2017-09-07 22:52:54 +0800

    增加社团实体

---
+++ 01_Website/ECommerce.Entity/Member/Society.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ECommerce.Entity.Member
+{
+    public class Society
+    {
+
+        public int Sys_No { get; set; }
+        public string SocietyID { get; set; }
+        public int OrganizationID { get; set; }
+        public string SocietyName { get; set; }
+        public string Pwd { get; set; }
+        public string Phone { get; set; }
+        public string Description { get; set; }
+        public string Status { get; set; }
+        public DateTime InDate { get; set; }
+        public string InUser { get; set; }
+        public DateTime EditDate { get; set; }
+        public string EditUser { get; set; }
+    }
+}
---
+++ 01_Website/ECommerce.UI/Controllers/SocietyController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ECommerce.UI.Controllers
+{
+    public class SocietyController : Controller
+    {
+        //
+        // GET: /Society/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Register()
+        {
+            return View();
+        }
+    }
+}
